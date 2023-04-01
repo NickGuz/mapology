427e052c2a9f5e3719050cd5e10fba31eeef10a4
Kevin Zhou <kevin.zhou.3@stonybrook.edu>
2023-04-01 17:35:11 -0400

    Create basic frontend tests using cypress

---
+++ client/src/components/RegisterScreen.cy.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import RegisterScreen from './RegisterScreen'
+
+describe('<RegisterScreen />', () => {
+  //Check all elements are rendered on page
+  it('Renders', () => {
+    cy.mount(<RegisterScreen />)
+    cy.get('.MuiTypography-h5').should('exist')
+    cy.get('.MuiTypography-subtitle2').should('exist')
+    cy.get('input[name="Username"]').should('exist')
+    cy.get('input[name="email"]').should('exist')
+    cy.get('input[name="password"]').should('exist')
+    cy.get('input[name="ConfrimPassword"]').should('exist')
+    cy.get('.PrivateSwitchBase-input').should('exist')
+    cy.get('.MuiFormControlLabel-root > .MuiTypography-root').should('exist')
+    cy.get('.MuiButton-root').should('exist')
+    cy.get('.MuiGrid-root > .MuiTypography-root').should('exist')
+    })
+
+  //Check all rendered elements have default values
+  it('Default Rendered Values', () => {
+    cy.mount(<RegisterScreen />)
+    cy.get('.MuiTypography-h5').should('contains.text', 'Create Your Account')
+    cy.get('.MuiTypography-subtitle2').should('contains.text', 'Create an account to save and view your maps')
+    cy.get('input[name="Username"]').should('have.value', '')
+    cy.get('input[name="email"]').should('have.value', '')
+    cy.get('input[name="password"]').should('have.value', '')
+    cy.get('input[name="ConfrimPassword"]').should('have.value', '')
+    cy.get('.PrivateSwitchBase-input').should('not.be.checked')
+    cy.get('.MuiFormControlLabel-root > .MuiTypography-root').should('contains.text', 'I agree to the Terms of Service and Privacy Policy')
+    cy.get('.MuiButton-root').should('contains.text', 'Create Your Account')
+    cy.get('.MuiGrid-root > .MuiTypography-root').should('contains.text', 'Already have an account? Sign in')
+  })
+
+})
---
+++ client/src/components/RegisterScreen.js
@@ -0,0 +1,138 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Link from '@mui/material/Link';
+
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  
+
+export default function RegisterScreen() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+     
+    };
+
+    return (
+        <Box sx = {{flexGrow:1}}>
+            <Grid container spacing={1}>
+                <Grid item xs= {4}>
+                    
+                </Grid>
+                <Grid item xs= {8} >
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline />
+                        <Box
+                            sx={{
+                                marginTop: 20,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                           
+                            <Typography component="h1" variant="h5">
+                                Create Your Account
+                            </Typography>
+                            <Typography component="h1" variant="subtitle2">
+                                Create an account to save and view your maps
+                            </Typography>
+                            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} >
+                                        <TextField
+                                            autoComplete="fname"
+                                            name="Username"
+                                            required
+                                            fullWidth
+                                            id="Username"
+                                            label=" Username"
+                                            autoFocus
+                                        />
+                                    </Grid>
+                                   
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            id="email"
+                                            label="Email Address"
+                                            name="email"
+                                            autoComplete="email"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            name="password"
+                                            label="Password"
+                                            type="password"
+                                            id="password"
+                                            autoComplete="new-password"
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            name="ConfrimPassword"
+                                            label="Confrim Password"
+                                            type="password"
+                                            id="ConfirmPassword"
+                                            autoComplete="new-password"
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <FormGroup >
+                                    <FormControlLabel control={<Checkbox  />} 
+                                    label={<Typography variant="body2" color="blue">I agree to the Terms of Service and Privacy Policy</Typography>}
+                                    
+                                    />
+                                </FormGroup>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Create Your Account
+                                </Button>
+                                <Grid container justifyContent="flex-end">
+                                    <Grid item>
+                                        <Link href="/login/" variant="body2">
+                                            Already have an account? Sign in
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Container>
+                </Grid>
+            </Grid>
+        </Box>
+            
+    );
+}
