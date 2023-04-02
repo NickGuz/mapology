a1e16828e8c393ef67a59ed80a65245171cec748
xela-h <xelah3@gmail.com>
2023-04-02 00:01:18 -0400

    added new directories and GET
    PUT doesn't work and sequelize wasn't working properly, kept getting null values for password

--- server/server.js
+++ server/server.js
@@ -1,19 +1,54 @@
 const express = require("express");
-const mysql = require("mysql");
 require("dotenv").config();
 const cors = require("cors");
-const routes = require("./AuthRouter");
+const db = require("./models/db")
+// const routes = require("./routers/AuthRouter");
 
 const app = express();
 app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(cors());
 
 // route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Mapology." });
+// app.use("/", routes);
+
+app.get("/register", (req, res) => {
+  db.query("SELECT * FROM users_test", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
-app.use("/", routes);
+app.post("/register", (req, res) => {
+  const insertQuery = "INSERT INTO users_test SET ?";
+
+  db.query(insertQuery, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send("User Added to Database");
+    }
+  });
+});
+
+// currently doesn't work
+app.put("/register", (req, res) => {
+  const updateQuery =
+    "UPDATE users_test SET email = ?, username = ?, password = ?, WHERE id = ?";
+  db.query(
+    updateQuery,
+    [req.body.email, req.body.username, req.body.password, req.body.id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
