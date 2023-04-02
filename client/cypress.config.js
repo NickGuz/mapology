1ce7c2267a523adaee48f70ea924a5a3f550cf5a
Kevin Zhou <kevin.zhou.3@stonybrook.edu>
2023-04-01 20:52:02 -0400

    Added Cypress E2E Testing

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -3,10 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1024,
   viewportHeight: 768,
+
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
