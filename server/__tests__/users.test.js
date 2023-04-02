7cb45c071132b61b093112260deccfcb605fd015
NickGuz <niickguz@gmail.com>
2023-04-02 16:37:20 -0400

    Fix some tests and include GET /users test

--- server/__tests__/users.test.js
+++ server/__tests__/users.test.js
@@ -1,13 +1,13 @@
 const supertest = require('supertest');
-const router = require('../AuthRouter');
-const { Users, sequelize } = require('../Users');
+const server = require('../server');
+const { User, sequelize } = require('../sequelizeTest');
 
-const app = router;
+const app = server;
 
 /** 
  * Test register a new user
  */
-test("POST /register", async () => {
+test("POST /users", async () => {
     const data = {
         email: "test@test.com",
         username: "testuser",
@@ -15,21 +15,21 @@ test("POST /register", async () => {
     };
 
     await supertest(app)
-        .post("/register")
+        .post("/users")
         .send(data)
         .expect(200)
         // can also test stuff like this but ignoring for now
         // .expect('Content-Type', /json/)
         // .expect('Content-Length', '15')
         .then(async (response) => {
-            // Check the response (Jest stuff in this block I think)
+            // Check the response (Jest stuff in this block)
             expect(response.body.id).toBeTruthy();
             expect(response.body.email).toBe(data.email);
-            expect(response.body.title).toBe(data.username);
+            expect(response.body.username).toBe(data.username);
             expect(response.body.password).toBe(data.password);
 
             // Check the data in the database
-            const user = await Users.findOne({ where: { id: response.body.id }});
+            const user = await User.findOne({ where: { id: response.body.id }});
             expect(user).toBeTruthy();
             expect(user.email).toBe(data.email);
             expect(user.username).toBe(data.username);
@@ -40,25 +40,51 @@ test("POST /register", async () => {
 /**
  * Test get a user
  */
+// test("GET /users", async () => {
+//     const user = await User.create({ 
+//         email: "joe@test.com",
+//         username: "joetest",
+//         password: "1234"
+//      });
+
+//     await supertest(app)
+//         .get("/users/" + user.id)
+//         .expect(200)
+//         .then((response) => {
+//             expect(response.body.id).toBe(user.id);
+//             expect(response.body.email).toBe(user.email);
+//             expect(response.body.username).toBe(user.username);
+//             expect(response.body.password).toBe(user.password);
+//         });
+// });
+
+/** 
+ * Test get all users
+ */
 test("GET /users", async () => {
-    const user = await Users.create({ 
+    const user1 = await User.create({
         email: "joe@test.com",
         username: "joetest",
         password: "1234"
-     });
+    });
+
+    const user2 = await User.create({
+        email: "bob@test.com",
+        username: "bobtest",
+        password: "12345"
+    });
 
     await supertest(app)
-        .get("/users/" + user.id)
+        .get("/users")
         .expect(200)
-        .then((response) => {
-            expect(response.body.id).toBe(user.id);
-            expect(response.body.email).toBe(user.email);
-            expect(response.body.username).toBe(user.username);
-            expect(response.body.password).toBe(user.password);
+        .then(async (response) => {
+            expect(response).toBeTruthy();
         });
 });
 
-afterAll(() => {
-    // TODO Don't keep SQL connection in Users, this is just temporary
+afterAll((done) => {
     sequelize.close();
+    app.close();
+    server.close();
+    done();
 })
