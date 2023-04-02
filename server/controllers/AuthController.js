5c36a4b29905393204b2e5185f2bc37480b84d4f
xela-h <xelah3@gmail.com>
2023-04-02 13:52:44 -0400

    updated for sequelize

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -12,29 +12,6 @@ exports.logout = () => {
   // TODO
 };
 
-// exports.register = (req, res) => {
-//   // TODO
-//   const { email, username, pass } = req.body;
-
-//   // insert new user into database
-//   const user = {
-//     email,
-//     username,
-//     pass,
-//   };
-
-//   db.query("INSERT INTO users_test SET ?", user, (error, results) => {
-//     if (error) {
-//       console.log(error);
-//       res.status(500).send("Internal Server Error");
-//       return;
-//     }
-
-//     // registration successful
-//     res.status(200).send("Registration Successful");
-//   });
-// }
-
 exports.register = () => {
   // TODO
 };
--- server/server.js
+++ server/server.js
@@ -1,54 +1,24 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const db = require("./models/db")
+const db = require("./models/db");
+const User = require("./sequelize");
 // const routes = require("./routers/AuthRouter");
 
 const app = express();
 app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(cors());
+app.use(express.static(path.join(process.cwd() + "/build")));
 
-// route
-// app.use("/", routes);
+app.get('/register', async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+})
 
-app.get("/register", (req, res) => {
-  db.query("SELECT * FROM users_test", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-app.post("/register", (req, res) => {
-  const insertQuery = "INSERT INTO users_test SET ?";
-
-  db.query(insertQuery, req.body, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send("User Added to Database");
-    }
-  });
-});
-
-// currently doesn't work
-app.put("/register", (req, res) => {
-  const updateQuery =
-    "UPDATE users_test SET email = ?, username = ?, password = ?, WHERE id = ?";
-  db.query(
-    updateQuery,
-    [req.body.email, req.body.username, req.body.password, req.body.id],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
-});
+app.post('/register', async (req, res) => {
+  const users = await User.create(req.body);
+  res.json(users);
+})
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
