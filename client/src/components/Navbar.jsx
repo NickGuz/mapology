e07fe56b6fcf055e099df8272ac774ea5b9c4ec6
NickGuz <niickguz@gmail.com>
2023-04-04 22:36:53 -0400

    Add navbar; no functionality yet and styling not finished

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,276 @@
+import React, { useState } from 'react';
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import SearchIcon from '@mui/icons-material/Search';
+import { Button, InputBase } from '@mui/material';
+
+const Navbar = (props) => {
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleRegister = () => {
+        // close the user menu
+        setAnchorElUser(null);
+    }
+
+    const handleLogin = () => {
+        // close the user menu
+        setAnchorElUser(null);
+    }
+
+    const handleLogout = () => {
+        // close the user menu
+        setAnchorElUser(null);
+    }
+
+    const handleImport = () => {
+
+    }
+
+    const handleBrowse = () => {
+
+    }
+
+    const handleProfile = () => {
+
+    }
+
+    const handleSettings = () => {
+
+    }
+
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25)
+        },
+        marginLeft: 0,
+        marginRight: 10,
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto'
+        }
+    }));
+
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }));
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: '12ch',
+                '&:focus': {
+                    width: '20ch'
+                }
+            }
+        }
+    }));
+
+    const getGuestButtons = () => {
+        return (
+            <>
+            <MenuItem key={'Log In'} onClick={handleLogin}>
+                <Typography textAlign="center">Log In</Typography>
+            </MenuItem>
+            <MenuItem key={'Register'} onClick={handleRegister}>
+                <Typography textAlign="center">Register</Typography>
+            </MenuItem>
+            </>
+        )
+    }
+
+    const getLoggedInButtons = () => {
+        return (
+            <>
+            <MenuItem key={'Profile'} onClick={handleProfile}>
+                <Typography textAlign="center">Log In</Typography>
+            </MenuItem>
+            <MenuItem key={'Settings'} onClick={handleSettings}>
+                <Typography textAlign="center">Register</Typography>
+            </MenuItem>
+            <MenuItem key={'Log Out'} onClick={handleLogout}>
+                <Typography textAlign="center">Register</Typography>
+            </MenuItem>
+            </>
+        )
+    }
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontWeight: 700,
+                            letterSpacing: '.1rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Mapology
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton 
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuItem />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            <MenuItem key='Import' onClick={handleImport}>
+                                <Typography textAlign='center'>Import</Typography>
+                            </MenuItem>
+
+                            <MenuItem key='Browse' onClick={handleBrowse}>
+                                <Typography textAlign='center'>Browse</Typography>
+                            </MenuItem>
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontWeight: 700,
+                            letterSpacing: '.1rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        <Button
+                            key='Import'
+                            onClick={handleImport}
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >
+                            Import
+                        </Button>
+
+                        <Button
+                            key='Browse'
+                            onClick={handleBrowse}
+                            sx={{ my: 2, color: 'white', display: 'block' }}
+                        >
+                            Browse
+                        </Button>
+                    </Box>
+
+                    <Search sx={{ flexGrow: 0 }}>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search..."
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Avatar" src="" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {props.isLoggedIn
+                            ? getLoggedInButtons()
+                            : getGuestButtons()}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+
+export default Navbar;
