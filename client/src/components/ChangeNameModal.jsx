7cff4a3445bc036abd0086225e092f6c595d9b5a
Michael Zhang <m1cha3lzhang@gmail.com>
2023-04-08 18:16:53 -0400

    map editing WIP

---
+++ client/src/components/ChangeNameModal.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const ChangeNameModal = (props) => {
+    const [name, setName] = useState(props.name);
+
+    function handleChangeName(event) {
+        console.log(name);
+        
+        if (name !== ""){
+            props.layer.bindTooltip(name, {className:"countryLabel", permanent: true, opacity: 0.7, direction: "center"}).openTooltip();
+            setName("");
+
+        }
+        
+        props.close();
+    }
+    function changeName (event){
+        setName(event.target.value);
+    }
+  
+    return (
+        <div>
+        <Modal
+                open = {props.show}
+                onClose = {props.close}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Rename Feature
+                </Typography>
+                <input 
+                    id="name-textfield" 
+                    type="text" 
+                    defaultValue={name} 
+                    onChange = {changeName}
+                />
+                <Box id="confirm-cancel-container">
+                    <Button
+                        id="change-name-button"
+                        className="modal-button"
+                        onClick={handleChangeName}
+                    >Confirm</Button>
+                    <Button
+                        id="cancel-button"
+                        className="modal-button"
+                        onClick={props.close}
+                    >Cancel</Button>
+                        
+                    </Box>
+                </Box>
+            </Modal>
+        </div>
+    );
+};
+
+export default ChangeNameModal;
--- client/src/components/MapCard.jsx
+++ client/src/components/MapCard.jsx
@@ -1,47 +1,63 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Chip from '@mui/material/Chip';
-import { Button, CardActions, Typography } from '@mui/material';
+import { Button, ButtonBase, CardActions, Typography } from '@mui/material';
 
 const MapCard = (props) => {
-
+    const navigate = useNavigate();
     const handleTagClick = (event) => {
+    }
+    const handleCardClick = (event) => {
+    }
+
+    const handleOpenEdit = (event) =>{
+        navigate("/map-editor/");
 
     }
 
     return (
         <Card variant="outlined" sx={{ maxWidth: 400 }}>
             <CardMedia
-                sx={{ height: 140 }}
-                image={props.image} 
-                title="map"
+                    sx={{ height: 140 }}
+                    image={props.image} 
+                    title="map"
             />
-            <CardContent sx={{ paddingBottom: 0 }}>
-                <Typography gutterBottom variant="h5" component="div">
-                    {props.title || 'Map'}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {props.description || "Description"}
-                </Typography>
-                {props.author &&
-                <Typography variant="body2" color="text.secondary">
-                    by {props.author}
-                </Typography>}
+            <ButtonBase
+                onClick={handleCardClick}
+                sx={{ width: '100%' }}
+                style={{justifyContent: "flex-start"}}
+            >
+                
+                <CardContent  sx={{ paddingBottom: 0 }}>
+                    <Typography gutterBottom variant="h5" component="div" align='left'>
+                        {props.title || 'Map'}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" align='left'>
+                        {props.description || "Description"}
+                    </Typography>
+                    {props.author &&
+                    <Typography variant="body2" color="text.secondary" align='left'>
+                        by {props.author}
+                    </Typography>}
 
-                {props.tags.map((tag) => (
-                    <Chip 
-                        key={tag}
-                        sx={{marginTop: '8px', marginRight: '4px'}} 
-                        label={tag} 
-                        onClick={handleTagClick} 
-                    />
-                ))}
-            </CardContent>
+                    
+                </CardContent>
+            </ButtonBase>
+            {props.tags.map((tag) => (
+                        <Chip 
+                            key={tag}
+                            sx={{marginTop: '4px', marginRight: '4px', marginLeft: '4px'}} 
+                            label={tag} 
+                            onClick={handleTagClick} 
+                        />
+                    ))}
             <CardActions>
                 <Button size="small">Duplicate</Button>
                 <Button size="small">Details</Button>
+                <Button onClick={handleOpenEdit} size="small">Open Editor</Button>
             </CardActions>
         </Card> 
     )
