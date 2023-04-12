b64140d5985593c2ec577d499ca0b9726f89dae6
NickGuz <niickguz@gmail.com>
2023-04-11 23:46:08 -0400

    Try wildcard route for Heroku client-side routing

--- server/server.js
+++ server/server.js
@@ -14,20 +14,27 @@ app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(cors({ credentials: true, origin: url }));
 app.use(express.static(path.join(process.cwd() + "/build")));
 
+
 app.get('/users', async (req, res) => {
-  const users = await User.findAll();
-  res.json(users);
+    const users = await User.findAll();
+    res.json(users);
 })
 
 app.post('/users', async (req, res) => {
-  const users = await User.create(req.body);
-  res.json(users);
+    const users = await User.create(req.body);
+    res.json(users);
+})
+
+// Allow client-side routing on Heroku
+// KEEP THIS AS THE LOWERMOST ROUTE
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'build', 'index.html'));
 })
 
 // set port, listen for requests
 const PORT = process.env.PORT || 4000;
 const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
 
 module.exports = server;
