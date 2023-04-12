51ef6fea5e2df1cb37fd66caf31bee82e8bb9302
NickGuz <niickguz@gmail.com>
2023-04-12 00:26:01 -0400

    One last attempt for now to fix heroku routing

--- server/server.js
+++ server/server.js
@@ -14,15 +14,31 @@ app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(cors({ credentials: true, origin: url }));
 app.use(express.static(path.join(process.cwd() + "/build")));
 
+// List of all the files that should be served as-is
+const protectedFiles = ['transformed.js', 'main.js', 'main.css', 'favicon.ico'];
+
 app.get('/users', async (req, res) => {
     const users = await User.findAll();
     res.json(users);
-})
+});
 
 app.post('/users', async (req, res) => {
     const users = await User.create(req.body);
     res.json(users);
-})
+});
+
+app.get('*', (req, res) => {
+    let route = req.params['0'].substring(1);
+    console.log('req.params', req.params);
+
+    if (protectedFiles.includes(route)) {
+        // Return the actual file
+        res.sendFile(path.join(process.cwd(), 'build', route));
+    } else {
+        // Otherwise, redirect to /build/index.html
+        res.sendFile(path.join(process.cwd(), 'build', 'index.html'));
+    }
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 4000;
