9cd2f09624daa94326380e6aaa4fc644811664d4
NickGuz <niickguz@gmail.com>
2023-04-15 15:26:30 -0400

    Allow importing of shp/dbf files; Change Features Geometry column to MEDIUMTEXT (was failing when files were too large)

--- client/src/components/RegionEditor.jsx
+++ client/src/components/RegionEditor.jsx
@@ -1,64 +1,61 @@
-import React from 'react'
+import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { BlockPicker } from "react-color";
 import Slider from "@mui/material/Slider";
 
 const RegionEditor = () => {
-
     const [value, setValue] = React.useState(50);
 
     const handleSliderChange = (event, newValue) => {
-      setValue(newValue);
+        setValue(newValue);
     };
 
-  return (
-    <Box>
-      <p
-        id="text-header"
-        style={{
-          paddingTop: "0px",
-          color: "dimgray",
-          fontSize: "120%",
-          borderTop: "1px solid",
-          borderBottom: "1px solid",
-          borderColor: "darkgray",
-          backgroundColor: "silver",
-        }}
-      >
-        Color
-      </p>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <BlockPicker />
-      </div>
-      <p
-        id="text-header"
-        style={{
-          paddingTop: "0px",
-          color: "dimgray",
-          fontSize: "120%",
-          borderTop: "1px solid",
-          borderBottom: "1px solid",
-          borderColor: "darkgray",
-          backgroundColor: "silver",
-        }}
-      >
-        Opacity
-      </p>
-      <Grid item xs sx={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ width: 200, marginRight: 20 }}>
-          <Slider
-            value={value}
-            onChange={handleSliderChange}
-            aria-label="opacity slider"
-          />
-        </div>
-        <div>{value}</div>
-      </Grid>
-    </Box>
-  );
-}
+    return (
+        <Box>
+            <p
+                id="text-header"
+                style={{
+                    paddingTop: "0px",
+                    color: "dimgray",
+                    fontSize: "120%",
+                    borderTop: "1px solid",
+                    borderBottom: "1px solid",
+                    borderColor: "darkgray",
+                    backgroundColor: "silver",
+                }}
+            >
+                Color
+            </p>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <BlockPicker />
+            </div>
+            <p
+                id="text-header"
+                style={{
+                    paddingTop: "0px",
+                    color: "dimgray",
+                    fontSize: "120%",
+                    borderTop: "1px solid",
+                    borderBottom: "1px solid",
+                    borderColor: "darkgray",
+                    backgroundColor: "silver",
+                }}
+            >
+                Opacity
+            </p>
+            <Grid item xs sx={{ display: "flex", justifyContent: "center" }}>
+                <div style={{ width: 200, marginRight: 20 }}>
+                    <Slider
+                        value={value}
+                        onChange={handleSliderChange}
+                        aria-label="opacity slider"
+                    />
+                </div>
+                <div>{value}</div>
+            </Grid>
+        </Box>
+    );
+};
 
-export default RegionEditor
-
-
+export default RegionEditor;
