8550aae29abbc7efb49f20a5853441d7357da06d
NickGuz <niickguz@gmail.com>
2023-04-16 00:54:39 -0400

    Add kinda scuffed settings modal

--- client/src/components/SettingsModal.jsx
+++ client/src/components/SettingsModal.jsx
@@ -1,7 +1,11 @@
 import { useContext } from "react";
-import { Dialog, DialogTitle, List, ListItem } from "@mui/material";
+import {Avatar, Box, Button, Dialog, DialogTitle, List, ListItem, ListItemAvatar} from "@mui/material";
 import ListItemText from "@mui/material/ListItemText";
 import GlobalStoreContext from "../store/store";
+import SketchColorPicker from './SketchColorPicker';
+
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 const SettingsModal = () => {
   const { store } = useContext(GlobalStoreContext);
@@ -10,13 +14,58 @@ const SettingsModal = () => {
     store.setOpenSettingsModal(false);
   };
 
+  const handleColorPickerChange = () => {
+
+  }
+
   return (
-    <Dialog onClose={handleClose} open={store.settingsModalOpen}>
+    <Dialog
+      onClose={handleClose}
+      open={store.settingsModalOpen}
+      fullWidth
+      maxWidth="sm"
+    >
       <DialogTitle>Settings</DialogTitle>
-      <List sx={{ pr: 0 }}>
-        <ListItem disableGutters>
+      <List
+        sx={{
+          pr: 0,
+        }}
+      >
+
+        <ListItem divider>
+          <SentimentSatisfiedAltIcon />
           <ListItemText>Profile</ListItemText>
         </ListItem>
+
+        <ListItem>
+          <ListItemText>Banner Here</ListItemText>
+        </ListItem>
+
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar/>
+          </ListItemAvatar>
+          <Button sx={{ ml: '30%' }}>Edit Bio</Button>
+        </ListItem>
+
+        <ListItem>
+          <ListItemText>Color Theme</ListItemText>
+          <SketchColorPicker/>
+        </ListItem>
+
+        <ListItem divider>
+          <AccountBoxIcon />
+          <ListItemText>Account Information</ListItemText>
+        </ListItem>
+
+        <ListItem>
+          <Button>Change Username</Button>
+        </ListItem>
+
+        <ListItem>
+          <Button>Change Password</Button>
+        </ListItem>
+
       </List>
     </Dialog>
   );
