7aff014640c5e1675afae7416e953c2c9c46adc4
NickGuz <niickguz@gmail.com>
2023-05-09 00:56:05 -0400

    Change getAllMaps query to link other tables so MapCard can now just do 1 query per

--- client/src/components/util/MapCard.jsx
+++ client/src/components/util/MapCard.jsx
@@ -7,104 +7,70 @@ import Chip from '@mui/material/Chip';
 import Link from '@mui/material/Link';
 import Italy from '../../assets/italy.gif';
 import { Box, Button, CardActions, Typography } from '@mui/material';
-import api from '../../auth/auth-request-api/AuthRequestApi';
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
-import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import {
   duplicateMap,
-  getTagsByMapId,
-  getThumbnail, 
-  hasLike, 
-  addLike, 
-  getAllMapLikes, 
-  deleteLike, 
-  hasDislike, 
-  addDislike, 
-  getAllMapDislikes, 
-  deleteDislike 
-
+  hasLike,
+  addLike,
+  getAllMapLikes,
+  deleteLike,
+  hasDislike,
+  addDislike,
+  getAllMapDislikes,
+  deleteDislike,
 } from '../../store/GlobalStoreHttpRequestApi';
-import GlobalStoreContext from '../../store/store';
+// import GlobalStoreContext from '../../store/store';
 import AuthContext from '../../auth/AuthContextProvider';
-import {IconButton} from '@mui/material';
+import { IconButton } from '@mui/material';
 
 const MapCard = (props) => {
-  const [author, setAuthor] = useState(null);
-  const [tags, setTags] = useState([]);
-  const [image, setImage] = useState(null);
   const [likes, setLikes] = useState({});
   const [userLike, setUserLike] = useState(false);
   const [dislikes, setDislikes] = useState({});
   const [userDislike, setUserDislike] = useState(false);
   const navigate = useNavigate();
-  const { store } = useContext(GlobalStoreContext);
+  // const { store } = useContext(GlobalStoreContext);
   const { auth } = useContext(AuthContext);
 
   useEffect(() => {
-    const getAuthorData = async () => {
-      let res = await api.getUserById(props.data.authorId);
-      setAuthor(res.data);
-    };
-
-    const getTagsData = async () => {
-      let res = await getTagsByMapId(props.data.id);
-      setTags(res.data.data);
-    };
-
-    const getThumbnailData = async () => {
-      let res = await getThumbnail(props.data.id);
-      let blob = res.data;
-      blob = blob.slice(0, blob.size, 'image/png');
-      setImage(blob);
-    };
-
-    const getMapLikes = async () => {
-      let res = await getAllMapLikes(props.data.id);
-      setLikes(res.data);
-    }
-    const getMapDisikes = async () => {
-      let res = await getAllMapDislikes(props.data.id);
-      setDislikes(res.data);
-    }
-
-    getAuthorData();
-    getTagsData();
-    getThumbnailData();
-    getMapLikes();
-    getMapDisikes();
-  }, [store.currentMap]);
+    console.log('data', props.data);
+    setUserLike(props.data.likes.some((l) => l.userId === auth.user.id));
+    setUserDislike(props.data.dislikes.some((l) => l.userId === auth.user.id));
+    setLikes(props.data.likes);
+    setDislikes(props.data.dislikes);
+  }, []);
 
   useEffect(() => {
     const helper = async () => {
       const userLiked = await hasLike(auth.user.id, props.data.id);
-      if (userLiked.status == "404"){
+      if (userLiked.status == '404') {
         setUserLike(false);
-      }
-      else{
+      } else {
         setUserLike(true);
-      } 
-    }
+      }
+    };
     helper();
   }, [likes]);
 
   useEffect(() => {
     const helper = async () => {
       const userDisliked = await hasDislike(auth.user.id, props.data.id);
-      if (userDisliked.status == "404"){
+      if (userDisliked.status == '404') {
         setUserDislike(false);
-      }
-      else{
+      } else {
         setUserDislike(true);
       }
-    }
+    };
     helper();
   }, [dislikes]);
+
   const handleLike = () => {
     let liked = null;
     const helper = async () => {
       liked = await hasLike(auth.user.id, props.data.id);
-      if(liked.status == "404"){
-        if (userDislike){
+      if (liked.status == '404') {
+        if (userDislike) {
           await deleteDislike(auth.user.id, props.data.id);
           let allMapDislikes = await getAllMapLikes(props.data.id);
           setDislikes(allMapDislikes.data);
@@ -112,37 +78,35 @@ const MapCard = (props) => {
         await addLike(auth.user.id, props.data.id);
         let allMapLikes = await getAllMapLikes(props.data.id);
         setLikes(allMapLikes.data);
-      }
-      else if(liked.status == "200"){
+      } else if (liked.status == '200') {
         await deleteLike(auth.user.id, props.data.id);
         let allMapLikes = await getAllMapLikes(props.data.id);
         setLikes(allMapLikes.data);
       }
-      
     };
     helper();
   };
 
   const handleDislike = () => {
-    let disliked = null;
     const helper = async () => {
-      disliked = await hasDislike(auth.user.id, props.data.id);
-      if(disliked.status == "404"){
-        if (userLike){
+      if (!userDislike) {
+        if (userLike) {
           await deleteLike(auth.user.id, props.data.id);
           let allMapLikes = await getAllMapLikes(props.data.id);
           setLikes(allMapLikes.data);
+          setUserLike(false);
         }
+
         await addDislike(auth.user.id, props.data.id);
         let allMapDislikes = await getAllMapDislikes(props.data.id);
         setDislikes(allMapDislikes.data);
-      }
-      else if(disliked.status == "200"){
+        setUserDislike(true);
+      } else {
         await deleteDislike(auth.user.id, props.data.id);
         let allMapDislikes = await getAllMapDislikes(props.data.id);
         setDislikes(allMapDislikes.data);
+        setUserDislike(false);
       }
-      
     };
     helper();
   };
@@ -157,7 +121,7 @@ const MapCard = (props) => {
   };
 
   const handleClickUsername = () => {
-    navigate(`/profile/${author.id}`);
+    navigate(`/profile/${props.data.user.id}`);
   };
 
   const handleDuplicate = () => {
@@ -172,7 +136,13 @@ const MapCard = (props) => {
     <Card variant="outlined" sx={{ maxWidth: 400 }}>
       <CardMedia
         sx={{ height: 280 }}
-        image={image || Italy /*props.data.imgPath*/}
+        image={
+          props.data.thumbnail.image.slice(
+            0,
+            props.data.thumbnail.image.size,
+            'image/png'
+          ) || Italy /*props.data.imgPath*/
+        }
         title="map"
       />
       <CardContent sx={{ paddingBottom: 0 }}>
@@ -182,7 +152,7 @@ const MapCard = (props) => {
         <Typography variant="body2" color="text.secondary" align="left">
           {props.data.description || 'Description'}
         </Typography>
-        {author && (
+        {props.data.user && (
           <Typography
             variant="body2"
             color="text.secondary"
@@ -195,14 +165,14 @@ const MapCard = (props) => {
               color="inherit"
               style={{ cursor: 'pointer' }}
             >
-              {author.username}
+              {props.data.user.username}
             </Link>
           </Typography>
         )}
       </CardContent>
-      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-        {tags &&
-          tags.map((tag) => (
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        {props.data.tags &&
+          props.data.tags.map((tag) => (
             <Chip
               key={tag.tagName}
               sx={{ marginTop: '4px', marginRight: '4px', marginLeft: '4px' }}
@@ -210,17 +180,32 @@ const MapCard = (props) => {
               onClick={handleTagClick}
             />
           ))}
-          
-          <IconButton sx={{ ml: "auto", color: ((!auth.user)?'grey': (userLike?"blue":"black")) }} onClick={handleLike} disabled = {!auth.loggedIn}>
-            <ThumbUpIcon />
-          </IconButton>
-          <Typography sx={{paddingTop:0.75, fontSize:25}}>{likes.length}</Typography>
-          <IconButton sx={{  color: ((!auth.user)?'grey': (userDislike?"blue":"black")) }} onClick={handleDislike} disabled = {!auth.loggedIn}>
-            <ThumbDownIcon /> 
-          </IconButton>
-          <Typography sx={{paddingTop:0.75, fontSize:25}}>{dislikes.length}</Typography>
+
+        <IconButton
+          sx={{
+            ml: 'auto',
+            color: !auth.user ? 'grey' : userLike ? 'blue' : 'black',
+          }}
+          onClick={handleLike}
+          disabled={!auth.loggedIn}
+        >
+          <ThumbUpIcon />
+        </IconButton>
+        <Typography sx={{ paddingTop: 0.75, fontSize: 25 }}>
+          {likes.length - dislikes.length}
+        </Typography>
+        <IconButton
+          sx={{ color: !auth.user ? 'grey' : userDislike ? 'blue' : 'black' }}
+          onClick={handleDislike}
+          disabled={!auth.loggedIn}
+        >
+          <ThumbDownIcon />
+        </IconButton>
+        {/* <Typography sx={{ paddingTop: 0.75, fontSize: 25 }}>
+          {dislikes.length}
+        </Typography> */}
       </Box>
-        
+
       <CardActions>
         <Button
           onClick={handleDuplicate}
